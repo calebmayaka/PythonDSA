4fabe96d22eafb57dbc59e229cc670333294fadb
Caleb Mayaka <mayakaombogo254@gmail.com>
2025-02-07 23:23:14 +0300

    list methods

---
+++ data_structures/lists/lists_1.py
@@ -0,0 +1,55 @@
+list1 = [1,2,3,4,5,6]
+
+# add 7 at the end of the list
+list1.append(7)
+
+# removes all items in a list
+list1.clear()
+
+# adds the given list to the end of the other list
+list1.extend([1,2,3,4,5,6,7,8,9,10])
+
+# inserting an element at a specific index
+list1.insert(2,10)
+
+# removes the first occurance of 5
+list1.remove(5)
+
+# removes element at a specific index
+list1.pop(3)
+
+# finds the index of the first occurance of 5
+
+list1.index(3)
+
+# counts the occurances of a specific value
+list1.count(2)
+
+# copying the list
+
+elisti_enyia = list1.copy
+
+print (elisti_enyia)
+
+# retyurning the length of the list - len
+
+size = len(list1)
+
+# minimum value
+min_value = min(list1)
+
+# maximum value
+
+
+
+
+
+
+
+
+
+
+
+
+# deleting the whole list
+del list1
